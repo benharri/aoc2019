72123d091331c572799611b379772f025fb838e3
Ben Harris <ben@tilde.team>
2019-12-01 02:35:17 -0500

    day1 init

---
+++ Day1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc2019
+{
+    public class Day1
+    {
+        private static IEnumerable<int> lines = 
+            File.ReadLines("input/day1.in").Select(line => int.Parse(line));
+
+        private static int FuelCost(int weight) => weight / 3 - 2;
+
+        public static void Part1()
+        {
+            Console.WriteLine(lines.Select(num => FuelCost(num)).Sum());
+        }
+
+        private static int FullCost(int cost)
+        {
+            int total = 0, newcost = 0, tmp = cost;
+
+            while ((newcost = FuelCost(tmp)) >= 0)
+            {
+                total += newcost;
+                tmp = newcost;
+            }
+
+            return total;
+        }
+
+        public static void Part2()
+        {
+            Console.WriteLine(lines.Select(cost => FullCost(cost)).Sum());
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace aoc2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Part1();
+            Day1.Part2();
+        }
+    }
+}
